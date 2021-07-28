620eea7a5fb4f9c36a7b71656e097e0ef2a58ae2
braedenf <braedenleefoster@gmail.com>
2021-07-29 07:53:35 +1200

    Created Object type with draw and positioning functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,129 @@
+use tcod::colors::*;
+use tcod::console::*;
+
+// Window Size
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGHT: i32 = 50;
+
+// Size of map
+const MAP_WIDTH: i32 = SCREEN_WIDTH;
+const MAP_HEIGHT: i32 = SCREEN_HEIGHT - 5;
+
+// Map colours
+const COLOR_DARK_WALL: Color = Color { r: 0, g: 0, b: 100 };
+const COLOR_DARK_GROUND: Color = Color {
+    r: 50,
+    g: 50,
+    b: 150,
+};
+
+const LIMIT_FPS: i32 = 20; // maximum of 20 frames per second
+
+struct Tcod {
+    root: Root,
+    con: Offscreen,
+}
+
+// Generic object struct
+struct Object {
+    x: i32,
+    y: i32,
+    char: char,
+    color: Color,
+}
+
+impl Object {
+    pub fn new(x: i32, y: i32, char: char, color: Color) -> Self {
+        Object { x, y, char, color }
+    }
+
+    // Move by the given amount
+    pub fn move_by(&mut self, dx: i32, dy: i32) {
+        self.x += dx;
+        self.y += dy;
+    }
+
+    //set the colour and then draw the character that represents the object at its position
+    pub fn draw(&self, con: &mut dyn Console) {
+        con.set_default_foreground(self.color);
+        con.put_char(self.x, self.y, self.char, BackgroundFlag::None);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let root = Root::initializer()
+        .font("arial10x10.png", FontLayout::Tcod)
+        .font_type(FontType::Greyscale)
+        .size(SCREEN_WIDTH, SCREEN_HEIGHT)
+        .title("Rouguelike Libtcod Tutorial")
+        .init();
+
+    let con = Offscreen::new(SCREEN_WIDTH, SCREEN_HEIGHT);
+
+    let mut tcod = Tcod { root, con };
+
+    tcod::system::set_fps(LIMIT_FPS);
+
+    // Setup player object
+    let player = Object::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, '@', WHITE);
+
+    // Setup NPC object
+    let npc = Object::new(SCREEN_WIDTH / 2 - 5, SCREEN_HEIGHT / 2, '@', YELLOW);
+
+    let mut objects = [player, npc];
+
+    // Game Loop
+    while !tcod.root.window_closed() {
+        tcod.con.clear();
+
+        //Draw objects to console
+        for object in &objects {
+            object.draw(&mut tcod.con);
+        }
+        //Add the character console render to root window
+        blit(
+            &tcod.con,
+            (0, 0),
+            (SCREEN_WIDTH, SCREEN_HEIGHT),
+            &mut tcod.root,
+            (0, 0),
+            1.0,
+            1.0,
+        );
+        tcod.root.flush(); // Places contents onto screen
+        tcod.root.wait_for_keypress(true);
+
+        // Modify player position with keyboard input until exit key has been pressed
+        let player = &mut objects[0];
+        let exit = handle_keys(&mut tcod, player);
+        if exit {
+            break;
+        }
+    }
+}
+
+fn handle_keys(tcod: &mut Tcod, player: &mut Object) -> bool {
+    use tcod::input::Key;
+    use tcod::input::KeyCode::*;
+
+    let key = tcod.root.wait_for_keypress(true);
+    match key {
+        Key {
+            code: Enter,
+            alt: true,
+            ..
+        } => {
+            let fullscreen = tcod.root.is_fullscreen();
+            tcod.root.set_fullscreen(!fullscreen);
+        }
+        Key { code: Escape, .. } => return true, // Exit game
+        Key { code: Up, .. } => player.move_by(0, -1),
+        Key { code: Down, .. } => player.move_by(0, 1),
+        Key { code: Left, .. } => player.move_by(-1, 0),
+        Key { code: Right, .. } => player.move_by(1, 0),
+
+        _ => {}
+    }
+
+    false
 }
